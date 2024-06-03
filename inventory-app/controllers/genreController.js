b5e757889c965ccb25439a30aab7e34270cc2bf5
danny <dannytokarev@gmail.com>
2024-06-03 12:22:43 -0400

    changed name to title

--- inventory-app/controllers/genreController.js
+++ inventory-app/controllers/genreController.js
@@ -8,7 +8,7 @@ const asyncHandler = require("express-async-handler");
 exports.genre_list = asyncHandler(async (req, res, next) => {
 	const allGenres = await Genre.find().sort({ name: 1 }).exec();
 	res.render("genre_list", {
-		name: "Genre List",
+		title: "Genre List",
 		genre_list: allGenres,
 	});
 });
