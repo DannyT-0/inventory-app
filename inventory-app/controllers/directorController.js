51282d9ead7280a035df2458d1cf9074ccd2437a
danny <dannytokarev@gmail.com>
2024-06-03 12:20:37 -0400

    finished basic functionality. errors still need fixing

--- inventory-app/controllers/directorController.js
+++ inventory-app/controllers/directorController.js
@@ -1,42 +1,221 @@
-const director = require("../models/director");
+const Director = require("../models/director");
+const Movie = require("../models/movie");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all directors.
 exports.director_list = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: director list");
+	const allDirectors = await Director.find().sort({ family_name: 1 }).exec();
+	res.render("director_list", {
+		title: "Director List",
+		director_list: allDirectors,
+	});
 });
 
 // Display detail page for a specific director.
 exports.director_detail = asyncHandler(async (req, res, next) => {
-	res.send(`NOT IMPLEMENTED: director detail: ${req.params.id}`);
+	// Get details of director and all their movies (in parallel)
+	const [director, allMoviesByDirector] = await Promise.all([
+		Director.findById(req.params.id).exec(),
+		Movie.find({ director: req.params.id }, "title summary").exec(),
+	]);
+
+	if (director === null) {
+		// No results.
+		const err = new Error("Director not found");
+		err.status = 404;
+		return next(err);
+	}
+
+	res.render("director_detail", {
+		title: "Director Detail",
+		director: director,
+		director_movies: allMoviesByDirector,
+	});
 });
 
-// Display director create form on GET.
-exports.director_create_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: director create GET");
-});
+// Display Director create form on GET.
+exports.director_create_get = (req, res, next) => {
+	res.render("director_form", { title: "Create Director", director: director });
+};
 
-// Handle director create on POST.
-exports.director_create_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: director create POST");
-});
+// Handle Director create on POST.
+exports.director_create_post = [
+	// Validate and sanitize fields.
+	body("first_name")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("First name must be specified.")
+		.isAlphanumeric()
+		.withMessage("First name has non-alphanumeric characters."),
+	body("family_name")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("Family name must be specified.")
+		.isAlphanumeric()
+		.withMessage("Family name has non-alphanumeric characters."),
+	body("date_of_birth", "Invalid date of birth")
+		.optional({ values: "falsy" })
+		.isISO8601()
+		.toDate(),
+	body("date_of_death", "Invalid date of death")
+		.optional({ values: "falsy" })
+		.isISO8601()
+		.toDate(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create Director object with escaped and trimmed data
+		const director = new Director({
+			first_name: req.body.first_name,
+			family_name: req.body.family_name,
+			date_of_birth: req.body.date_of_birth,
+			date_of_death: req.body.date_of_death,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render("director_form", {
+				title: "Create Director",
+				director: director,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data from form is valid.
+
+			// Save director.
+			await director.save();
+			// Redirect to new director record.
+			res.redirect(director.url);
+		}
+	}),
+];
 
 // Display director delete form on GET.
 exports.director_delete_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: director delete GET");
+	// Get details of director and all their movies (in parallel)
+	const [director, allMoviesByDirector] = await Promise.all([
+		Director.findById(req.params.id).exec(),
+		Movie.find({ director: req.params.id }, "title summary").exec(),
+	]);
+
+	if (director === null) {
+		// No results.
+		res.redirect("/catalog/directors");
+	}
+
+	res.render("director_delete", {
+		title: "Delete Director",
+		director: director,
+		director_movies: allMoviesByDirector,
+	});
 });
 
-// Handle director delete on POST.
+// Handle Director delete on POST.
 exports.director_delete_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: director delete POST");
+	// Get details of director and all their movies (in parallel)
+	const [director, allMoviesByDirector] = await Promise.all([
+		Director.findById(req.body.directorid).exec(),
+		Movie.find({ director: req.body.directorid }, "title summary").exec(),
+	]);
+
+	if (allMoviesByDirector.length > 0) {
+		// Director has movies. Render in same way as for GET route.
+		res.render("director_delete", {
+			title: "Delete Director",
+			director: director,
+			director_movies: allMoviesByDirector,
+		});
+		return;
+	} else {
+		// Director has no movies. Delete object and redirect to the list of directors.
+		await Director.findByIdAndDelete(req.body.directorid);
+		res.redirect("/catalog/directors");
+	}
 });
 
-// Display director update form on GET.
+// Display Director update form on GET.
 exports.director_update_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: director update GET");
+	// Get director details.
+	const director = await Director.findById(req.params.id).exec();
+
+	if (director === null) {
+		// No results.
+		const err = new Error("Director not found");
+		err.status = 404;
+		return next(err);
+	}
+
+	// Success. Render form with director details.
+	res.render("director_form", {
+		title: "Update Director",
+		director: director,
+	});
 });
 
 // Handle director update on POST.
-exports.director_update_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: director update POST");
-});
+exports.director_update_post = [
+	// Validate and sanitize fields.
+	body("first_name")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("First name must be specified.")
+		.isAlphanumeric()
+		.withMessage("First name has non-alphanumeric characters."),
+	body("family_name")
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage("Family name must be specified.")
+		.isAlphanumeric()
+		.withMessage("Family name has non-alphanumeric characters."),
+	body("date_of_birth", "Invalid date of birth")
+		.optional({ values: "falsy" })
+		.isISO8601()
+		.toDate(),
+	body("date_of_death", "Invalid date of death")
+		.optional({ values: "falsy" })
+		.isISO8601()
+		.toDate(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create director object with escaped and trimmed data (and the old id!)
+		const director = new Director({
+			first_name: req.body.first_name,
+			family_name: req.body.family_name,
+			date_of_birth: req.body.date_of_birth,
+			date_of_death: req.body.date_of_death,
+			_id: req.params.id, // This is required, or a new ID will be assigned!
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render("director_form", {
+				title: "Update Director",
+				director: director,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data from form is valid. Update the record.
+			const updatedDirector = await Director.findByIdAndUpdate(
+				req.params.id,
+				director,
+				{}
+			);
+			// Redirect to director detail page.
+			res.redirect(updatedDirector.url);
+		}
+	}),
+];
--- inventory-app/controllers/movieController.js
+++ inventory-app/controllers/movieController.js
@@ -3,9 +3,10 @@ const Director = require("../models/director");
 const Genre = require("../models/genre");
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
-	// Get details of movies, movie instances, authors and genre counts (in parallel)
+	// Get details of movies, movie instances, directors and genre counts (in parallel)
 	const [numMovies, numDirectors, numGenres] = await Promise.all([
 		Movie.countDocuments({}).exec(),
 		Director.countDocuments({}).exec(),
@@ -22,40 +23,260 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all movies.
 exports.movie_list = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: movie list");
+	const allMovies = await Movie.find({}, "title director")
+		.sort({ title: 1 })
+		.populate("director")
+		.exec();
+
+	res.render("movie_list", { title: "Movie List", movie_list: allMovies });
 });
 
 // Display detail page for a specific movie.
 exports.movie_detail = asyncHandler(async (req, res, next) => {
-	res.send(`NOT IMPLEMENTED: movie detail: ${req.params.id}`);
+	// Get details of movies for specific movie
+	const movie = await Promise.all([
+		Movie.findById(req.params.id).populate("director").populate("genre").exec(),
+	]);
+
+	if (movie === null) {
+		// No results.
+		const err = new Error("Movie not found");
+		err.status = 404;
+		return next(err);
+	}
+
+	res.render("movie_detail", {
+		title: movie.title,
+		movie: movie,
+	});
 });
 
 // Display movie create form on GET.
 exports.movie_create_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: movie create GET");
+	// Get all directors and genres, which we can use for adding to our movie.
+	const [allDirectors, allGenres] = await Promise.all([
+		Director.find().sort({ family_name: 1 }).exec(),
+		Genre.find().sort({ name: 1 }).exec(),
+	]);
+
+	res.render("movie_form", {
+		title: "Create Movie",
+		directors: allDirectors,
+		genres: allGenres,
+	});
 });
 
 // Handle movie create on POST.
-exports.movie_create_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: movie create POST");
-});
+exports.movie_create_post = [
+	// Convert the genre to an array.
+	(req, res, next) => {
+		if (!Array.isArray(req.body.genre)) {
+			req.body.genre =
+				typeof req.body.genre === "undefined" ? [] : [req.body.genre];
+		}
+		next();
+	},
+
+	// Validate and sanitize fields.
+	body("title", "Title must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("director", "Director must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("summary", "Summary must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("genre.*").escape(),
+	// Process request after validation and sanitization.
+
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create a Movie object with escaped and trimmed data.
+		const movie = new Movie({
+			title: req.body.title,
+			director: req.body.director,
+			summary: req.body.summary,
+			genre: req.body.genre,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/error messages.
+
+			// Get all directors and genres for form.
+			const [allDirectors, allGenres] = await Promise.all([
+				Director.find().sort({ family_name: 1 }).exec(),
+				Genre.find().sort({ name: 1 }).exec(),
+			]);
+
+			// Mark our selected genres as checked.
+			for (const genre of allGenres) {
+				if (movie.genre.includes(genre._id)) {
+					genre.checked = "true";
+				}
+			}
+			res.render("movie_form", {
+				title: "Create Movie",
+				directors: allDirectors,
+				genres: allGenres,
+				movie: movie,
+				errors: errors.array(),
+			});
+		} else {
+			// Data from form is valid. Save movie.
+			await movie.save();
+			res.redirect(movie.url);
+		}
+	}),
+];
 
 // Display movie delete form on GET.
+// Display movie delete form on GET.
 exports.movie_delete_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: movie delete GET");
+	// Get movie details
+	const movie = await Promise.all([
+		Movie.findById(req.params.id).populate("director").populate("genre").exec(),
+	]);
+
+	if (movie === null) {
+		// No results.
+		res.redirect("/catalog/movies");
+	}
+
+	res.render("movie_delete", {
+		title: "Delete Movie",
+		movie: movie,
+	});
 });
 
 // Handle movie delete on POST.
 exports.movie_delete_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: movie delete POST");
+	// Get movie details
+	const movie = await Promise.all([Movie.findById(req.body.movieid).exec()]);
+
+	// if (movieInstances.length > 0) {
+	// 	// movie has movie instances. Render in the same way as for GET route.
+	// 	res.render("movie_delete", {
+	// 		title: "Delete Movie",
+	// 		movie: movie,
+	// 		movie_instances: movieInstances,
+	// 	});
+	// 	return;
+	// }
+
+	// movie has no movie instances. Delete object and redirect to the list of movies.
+	await Movie.findByIdAndDelete(req.body.movieid);
+	res.redirect("/catalog/movies");
 });
 
 // Display movie update form on GET.
 exports.movie_update_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: movie update GET");
+	// Get movie, directors and genres for form.
+	const [movie, allDirectors, allGenres] = await Promise.all([
+		Movie.findById(req.params.id).populate("director").exec(),
+		Director.find().sort({ family_name: 1 }).exec(),
+		Genre.find().sort({ name: 1 }).exec(),
+	]);
+
+	if (movie === null) {
+		// No results.
+		const err = new Error("Movie not found");
+		err.status = 404;
+		return next(err);
+	}
+
+	// Mark our selected genres as checked.
+	allGenres.forEach((genre) => {
+		if (movie.genre.includes(genre._id)) genre.checked = "true";
+	});
+
+	res.render("movie_form", {
+		title: "Update Movie",
+		directors: allDirectors,
+		genres: allGenres,
+		movie: movie,
+	});
 });
 
 // Handle movie update on POST.
-exports.movie_update_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: movie update POST");
-});
+exports.movie_update_post = [
+	// Convert the genre to an array.
+	(req, res, next) => {
+		if (!Array.isArray(req.body.genre)) {
+			req.body.genre =
+				typeof req.body.genre === "undefined" ? [] : [req.body.genre];
+		}
+		next();
+	},
+
+	// Validate and sanitize fields.
+	body("title", "Title must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("director", "Director must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("summary", "Summary must not be empty.")
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body("isbn", "ISBN must not be empty").trim().isLength({ min: 1 }).escape(),
+	body("genre.*").escape(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create a movie object with escaped/trimmed data and old id.
+		const movie = new Movie({
+			title: req.body.title,
+			director: req.body.director,
+			summary: req.body.summary,
+			isbn: req.body.isbn,
+			genre: typeof req.body.genre === "undefined" ? [] : req.body.genre,
+			_id: req.params.id, // This is required, or a new ID will be assigned!
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/error messages.
+
+			// Get all directors and genres for form
+			const [allDirectors, allGenres] = await Promise.all([
+				Director.find().sort({ family_name: 1 }).exec(),
+				Genre.find().sort({ name: 1 }).exec(),
+			]);
+
+			// Mark our selected genres as checked.
+			for (const genre of allGenres) {
+				if (movie.genre.indexOf(genre._id) > -1) {
+					genre.checked = "true";
+				}
+			}
+			res.render("movie_form", {
+				title: "Update Movie",
+				directors: allDirectors,
+				genres: allGenres,
+				movie: movie,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data from form is valid. Update the record.
+			const updatedMovie = await Movie.findByIdAndUpdate(
+				req.params.id,
+				movie,
+				{}
+			);
+			// Redirect to movie detail page.
+			res.redirect(updatedMovie.url);
+		}
+	}),
+];
